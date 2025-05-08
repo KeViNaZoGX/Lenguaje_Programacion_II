deb74553896d651807fcbc9106a6635f1a98917b
MrKev <kevinleonardoolivasdelarosa@gmail.com>
2025-05-08 17:24:50 -0600

    Avance Proyecto Final

--- Proyecto_Final.cpp
+++ Proyecto_Final.cpp
@@ -51,13 +51,14 @@ int main(){
     InfoVendedor Vendedor;
     int opcion;
 
-    do {
+    /*
     FILE *archivo;
-    archivo = fopen("/home/mrkev/Documentos/Documentos/Archivos/Documentos/Documents/UACH/LENGUAJE DE PROGRAMACION II/ARCHIVOS/Parcial 3/Lenguaje_Programacion_II","w");
+    //archivo = fopen("/home/mrkev/Documentos/Documentos/Archivos/Documentos/Documents/UACH/LENGUAJE DE PROGRAMACION II/ARCHIVOS/Parcial 3/Lenguaje_Programacion_II","w");
     if (archivo == NULL) {
         printf("Error al abrir el archivo.\n");
         return 1;  // Retornar 1 en caso de error
-    }
+    }*/
+    do{
     limpiarPantalla();
     printf("CREACION DE LA FACTURA\n\n");
     printf("\nIngresar los Productos a Vender = 1");
